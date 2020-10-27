7d6dd5f4703be1fa871d8f40822aa29a3d9e8f4d
Wen Cong <S10194723@connect.np.edu.sg>
2020-10-27 11:57:53 +0800

    Completed exercise 3&4

--- MAD2_Practical2/ViewController.swift
+++ MAD2_Practical2/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var friendTextView: UITextView!
+    @IBOutlet weak var friendUnder20: UITextView!
+    @IBOutlet weak var avgAge: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -26,13 +28,43 @@ class ViewController: UIViewController {
         friendList.append(friend4)
         friendList.append(friend5)
         
-        var names:String = ""
-        for i in friendList{
-            names += "Name: \(i.firstName) \(i.lastName)    Age:\(i.age) \n"
-            
+        func displayNames(){
+            var names:String = ""
+            print("All Friends")
+            for i in friendList{
+                print(i.firstName + " " + i.lastName)
+                names += "Name: \(i.firstName) \(i.lastName)    Age:\(i.age) \n"
+                
+            }
+            friendTextView.text = names
         }
-        friendTextView.text = names
         
+        
+        func displayFriendsUnder20(){
+            var names:String = ""
+            print("Friend under 20")
+            for i in friendList{
+                if (i.age < 20){
+                    print(i.firstName + " " + i.lastName)
+                    names += "Name: \(i.firstName) \(i.lastName)    Age:\(i.age) \n"
+                }
+            }
+            friendUnder20.text = names
+        }
+        
+        func findAverageAge()->Double {
+            var sum:Int = 0
+            for i in friendList{
+                sum += i.age
+            }
+            var avg:Double = Double(sum/friendList.count)
+            return avg
+        }
+        
+        displayNames()
+        displayFriendsUnder20()
+        avgAge.text = "Average Age: \(findAverageAge())"
+        print(findAverageAge())
     }
     
     
