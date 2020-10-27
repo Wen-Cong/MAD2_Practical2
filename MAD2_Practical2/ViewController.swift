d00d9a1cbc355081d5097451bdf6490f65a9dd20
Wen Cong <S10194723@connect.np.edu.sg>
2020-10-27 10:13:16 +0800

    First commit

---
+++ MAD2_Practical2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MAD2_Practical2
+//
+//  Created by Yeo Wen Cong on 27/10/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
